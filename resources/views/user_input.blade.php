fcdacdc4a84c5c6a5cb5ac691e1f72506e6f9898
Reyhan Aditya <83198873+ReyNAditya@users.noreply.github.com>
2022-04-17 14:08:11 +0700

    Update Reyhan #03

--- resources/views/user_input.blade.php
+++ resources/views/user_input.blade.php
@@ -16,7 +16,7 @@
                                         <div class="col-md-12">
                                             <div class="form-group">
                                                 <label>Nama Lengkap</label>
-                                                <input type="text" class="form-control" id="nama" name="nama"placeholder="Risa Augusta Murti">
+                                                <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Lengkap">
                                                 @error('nama')
                                                 <div class="text-danger">{{$message}}</div>
                                                 @enderror
@@ -27,7 +27,7 @@
                                         <div class="col-md-12">
                                             <div class="form-group">
                                                 <label>Alamat</label>
-                                                <input type="text" class="form-control"id="alamat" name="alamat" placeholder="Jl. Mastrip Gg. Srikoyo 20, Kel. Jrebeng Wetan, Kec. Kedopok, RT 04 RW 04">
+                                                <input type="text" class="form-control"id="alamat" name="alamat" placeholder="Alamat Lengkap">
                                                 @error('alamat')
                                                 <div class="text-danger">{{$message}}</div>
                                                 @enderror
@@ -37,8 +37,8 @@
                                     <div class="row">
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label>Berat Sampah</label>
-                                                <input type="text" class="form-control" id="berat_sampah" name="berat_sampah" placeholder="3 (dalam kg)">
+                                                <label>Berat Laundry</label>
+                                                <input type="text" class="form-control" id="berat_sampah" name="berat_sampah" placeholder="(dalam kg)">
                                                 @error('berat_sampah')
                                                 <div class="text-danger">{{$message}}</div>
                                                 @enderror
@@ -58,9 +58,9 @@
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label for="tps">TPS</label>
+                                                <label for="tps">Tempat Laundry</label>
                                                 <select class="form-control" id="id_petugas" name="id_petugas">
-                                                  <option>-- Pilih TPS terdekat --</option>
+                                                  <option>-- Pilih Laundry terdekat --</option>
                                                   @foreach ($petugas as $item)
                                                   <option value="<?= $item->id_petugas ?>"><?= $item->nama_tps ?></option>
                                                  @endforeach
@@ -71,6 +71,77 @@
                                         <div class="text-danger">{{$message}}</div>
                                         @enderror
                                     </div>
+
+                                    <fieldset class="checkbox-group">
+  <legend class="checkbox-group-legend">JENIS LAUNDRY</legend>
+  <div class="checkbox">
+    <label class="checkbox-wrapper">
+      <input type="checkbox" class="checkbox-input" id="jenis_laundry" name="jenis_laundry" value="1" />
+      <span class="checkbox-tile">
+        <span class="checkbox-icon">
+          <img src="{{asset('/img/pakaian.png')}}" width="37px" height="37px">
+        </span>
+        <span class="checkbox-label">Pakaian</span>
+      </span>
+    </label>
+  </div>
+  <br>
+  <div class="checkbox">
+    <label class="checkbox-wrapper">
+      <input type="checkbox" class="checkbox-input" id="jenis_laundry" name="jenis_laundry" value="2" />
+      <span class="checkbox-tile">
+        <span class="checkbox-icon">
+          <img src="{{asset('/img/selimut.png')}}" width="37px" height="37px">
+        </span>
+        <span class="checkbox-label">Selimut</span>
+      </span>
+    </label>
+  </div>
+  <br>
+  <div class="checkbox">
+    <label class="checkbox-wrapper">
+      <input type="checkbox" class="checkbox-input" id="jenis_laundry" name="jenis_laundry" value="3" />
+      <span class="checkbox-tile">
+        <span class="checkbox-icon">
+            <img src="{{asset('/img/sprei.png')}}" width="37px" height="37px">
+        </span>
+        <span class="checkbox-label">Sprei</span>
+      </span>
+    </label>
+  </div>
+  <br>
+  <div class="checkbox">
+    <label class="checkbox-wrapper">
+      <input type="checkbox" class="checkbox-input" id="jenis_laundry" name="jenis_laundry" value="4" />
+      <span class="checkbox-tile">
+        <span class="checkbox-icon">
+        <img src="{{asset('/img/gorden.png')}}" width="37px" height="37px">
+        </span>
+        <span class="checkbox-label">Gorden</span>
+      </span>
+    </label>
+  </div>
+  <br>
+  <div class="checkbox">
+    <label class="checkbox-wrapper">
+      <input type="checkbox" class="checkbox-input" id="jenis_laundry" name="jenis_laundry" value="5" />
+      <span class="checkbox-tile">
+        <span class="checkbox-icon">
+        <img src="{{asset('/img/carpet.png')}}" width="37px" height="37px">
+        </span>
+        <span class="checkbox-label">Karpet</span>
+      </span>
+    </label>
+  </div>
+</fieldset>
+<br>
+<div class="checkbox-group">
+<legend class="checkbox-group-legend">JENIS SERVICE</legend>
+<input type="radio" class="btn-check" name="service" id="express" value="2" autocomplete="off">
+<label class="btn btn-success" for="express">Express</label>
+<input type="radio" class="btn-check" name="service" id="non-express" value="1" autocomplete="off">
+<label class="btn btn-danger" for="non-express">Non Express</label>
+</div>
 
                                     <button type="submit" class="btn btn-success btn-fill pull-right">Kirim</button>
                                     <div class="clearfix"></div>
